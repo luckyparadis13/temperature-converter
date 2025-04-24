907efe3472a0f365ec5e558137ed7fd83ec2acf7
Elizabeth Lucky Paradis <lucky.paradis13@gmail.com>
2025-04-23 20:28:43 -0700

    final answers

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-s;
+function convertToCelsius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
+}
+
+function describeTemperature(fahrenheit) {
+  const celsius = convertToCelsius(fahrenheit);
+
+  if (celsius < 0) {
+    return "very cold";
+  } else if (celsius < 20) {
+    return "cold";
+  } else if (celsius < 30) {
+    return "warm";
+  } else if (celsius < 40) {
+    return "hot";
+  } else {
+    return "very hot";
+  }
+}
+
+console.log(convertToCelsius(86)); // → 30
+console.log(describeTemperature(86)); // → "hot"
